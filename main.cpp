33f8964993d511695e545fe169d6363cea6ea9e7
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2020-09-17 14:51:26 -0700

    Created function to generate random double, added file IO to create file. added for loop to write int-double pairs into the file(first draft)

--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <random>
-using namespace std;
+#include <iomanip>
 
-void writeNumberToFile() {
-    ofstream file;
-    file.open ("Reading.txt");
-    file << "Please writr this text to a file.\n this text is written using C++\n";
-    file.close();
+using namespace std;
+const int intlowerbound {512};
+const int intupperbound{1024};
+const int dblLowerbound {50};
+const int dblUpperbound{90};
+
+double generateRandDbl() {
+    default_random_engine generator(time(0));
+    uniform_real_distribution<double>
+            distribution(dblLowerbound, dblUpperbound);
+    double randDbl = distribution(generator);
+    return randDbl;
+
 }
 int main() {
-    const int intlowerbound {512};
-    const int intupperbound{1024};
     random_device rd;
     mt19937 generator(rd()); //rd creates random seed
     uniform_int_distribution<>
             distribution(intlowerbound, intupperbound);
     int sequenceLimit  = distribution(generator);
     int sequence[sequenceLimit];
+    double sequencePair[sequenceLimit];
     for (int x = 0; x < sequenceLimit; x++) {
         sequence[x] = x;
     }
+    for (int x = 0; x < sequenceLimit;x++) {
+        sequencePair[x] = generateRandDbl();
+    }
+    ofstream file;
+    file.open ("Reading.txt");
+    for (int x = 0; x < sequenceLimit;x++){
+        file << sequence[x] << " " << setprecision(3) <<sequencePair[x] << " \n";
+    }
+    file.close();
     return 0;
 
 }
