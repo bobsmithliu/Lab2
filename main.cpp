79c105bb62c539c816318b5baed9da37dba9e6e3
bobsmithliu <55114292+bobsmithliu@users.noreply.github.com>
2020-09-17 14:27:20 -0700

    Created the random int list generation for loop.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <random>
+using namespace std;
 
+void writeNumberToFile() {
+    ofstream file;
+    file.open ("Reading.txt");
+    file << "Please writr this text to a file.\n this text is written using C++\n";
+    file.close();
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int intlowerbound {512};
+    const int intupperbound{1024};
+    random_device rd;
+    mt19937 generator(rd()); //rd creates random seed
+    uniform_int_distribution<>
+            distribution(intlowerbound, intupperbound);
+    int sequenceLimit  = distribution(generator);
+    int sequence[sequenceLimit];
+    for (int x = 0; x < sequenceLimit; x++) {
+        sequence[x] = x;
+    }
     return 0;
+
 }
